eba0f05a52568d796ab40fc8086e33fb45f942ad
mzazakeith <mzazakeith@gmail.com>
2022-03-10 13:16:07 +0300

    learnt about express, simple get requests and env variables

---
+++ express-demo/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("Hello World!!!");
+});
+app.get('/api/courses', (req,res)=>{
+    res.send([{id:1, name:"Science"},{id:2, name:"Math"} ]);
+});
+
+const port = process.env.PORT || 3001;
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}.....`)
+})
